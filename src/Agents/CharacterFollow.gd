89f0bc8b27e1551304797f8f0ad891ffc1652bef
RMdS-GitHub <rmds.dev@protonmail.com>
2020-11-18 16:04:29 +0100

    Making the AI follow the mouse.

--- src/Agents/CharacterFollow.gd
+++ src/Agents/CharacterFollow.gd
@@ -3,6 +3,8 @@ extends KinematicBody2D
 
 onready var sprite: Sprite = $TriangleRed
 
+const DISTANCE_TRESHOLD: = 3.0
+
 export var max_speed: = 500.0
 var _velocity: = Vector2.ZERO
 
@@ -10,6 +12,15 @@ var _velocity: = Vector2.ZERO
 func _physics_process(delta: float) -> void:
 	var target_global_position: Vector2 = get_global_mouse_position()
 	
-	_velocity = Vector2()
+	if global_position.distance_to(target_global_position) < DISTANCE_TRESHOLD:
+		return
 	
-	move_and_slide(_velocity)
+	_velocity = Steering.follow(
+		_velocity,
+		global_position,
+		target_global_position,
+		max_speed
+	)
+	
+	_velocity = move_and_slide(_velocity)
+	sprite.rotation = _velocity.angle()

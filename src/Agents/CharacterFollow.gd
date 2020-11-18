0efefceffc3f8e884a938639d52e35cb999cdd28
RMdS-GitHub <rmds.dev@protonmail.com>
2020-11-18 15:20:09 +0100

    Starting to code the follow AI.

--- src/Agents/CharacterFollow.gd
+++ src/Agents/CharacterFollow.gd
@@ -1,2 +1,15 @@
 extends KinematicBody2D
 
+
+onready var sprite: Sprite = $TriangleRed
+
+export var max_speed: = 500.0
+var _velocity: = Vector2.ZERO
+
+
+func _physics_process(delta: float) -> void:
+	var target_global_position: Vector2 = get_global_mouse_position()
+	
+	_velocity = Vector2()
+	
+	move_and_slide(_velocity)

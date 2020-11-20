63fd7f914febe6cdb39f5a8ad640b3e4ad079650
RMdS-GitHub <rmds.dev@protonmail.com>
2020-11-20 16:19:05 +0100

    Coding the arrow's movement.

--- src/UI/Target.gd
+++ src/UI/Target.gd
@@ -1,2 +1,27 @@
 extends Area2D
 
+
+onready var anim_player: AnimationPlayer = $AnimationPlayer
+
+
+func _ready() -> void:
+# warning-ignore:return_value_discarded
+	connect("body_entered", self, "_on_body_entered")
+	visible = false
+	
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_released("click"):
+		anim_player.play("fade_in")
+		
+		
+# warning-ignore:unused_argument
+func _physics_process(delta: float) -> void:
+	if Input.is_action_pressed("click"):
+		global_position = get_global_mouse_position()
+
+
+
+# warning-ignore:unused_argument
+func _on_body_entered(body: PhysicsBody2D) -> void:
+	anim_player.play("fade_out")

59a46c27524471e2f545f14e623135da1d2fbfeb
RMdS-GitHub <rmds.dev@protonmail.com>
2020-11-21 16:41:14 +0100

    Making AI's follow one another.

--- src/Agents/CharacterFollow.gd
+++ src/Agents/CharacterFollow.gd
@@ -2,24 +2,34 @@ extends KinematicBody2D
 
 
 onready var sprite: Sprite = $TriangleRed
+onready var target: = get_node(target_path)
 
-const DISTANCE_TRESHOLD: = 3.0
+const ARRIVE_TRESHOLD: = 3.0
+
+export var target_path: = NodePath()
+export var follow_offset: = 100.0
 
 export var max_speed: = 500.0
 var _velocity: = Vector2.ZERO
 
 
 func _physics_process(delta: float) -> void:
-	var target_global_position: Vector2 = get_global_mouse_position()
+	if target == self:
+		set_physics_process(false)
 	
-	if global_position.distance_to(target_global_position) < DISTANCE_TRESHOLD:
+	var target_global_position: Vector2 = target.global_position
+	
+	var to_target: = global_position.distance_to(target_global_position)
+	if to_target < ARRIVE_TRESHOLD:
 		return
 	
-	_velocity = Steering.follow(
+	_velocity = Steering.arrive_to(
 		_velocity,
 		global_position,
 		target_global_position,
-		max_speed
+		max_speed,
+		200.0,
+		20.0
 	)
 	
 	_velocity = move_and_slide(_velocity)
